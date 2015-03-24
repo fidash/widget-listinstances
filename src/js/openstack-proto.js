77b8cf88d28a6307c0e668701c7002328aa1b9b9
bgrana <bgrana@conwet.com>
2015-03-24 17:10:25 +0100

    Add loading animation during getInstanceList first request

--- src/js/openstack-proto.js
+++ src/js/openstack-proto.js
@@ -34,6 +34,9 @@ var OpenStackListInstance = (function (JSTACK) {
         // Initialize Keystone
         JSTACK.Keystone.init(url);
 
+        // Start loading animation
+        startLoadingAnimation();
+
         // Get tenants with the user's FIWARE token
         MashupPlatform.http.makeRequest(url + 'tenants', {
             method: 'GET',
@@ -56,7 +59,14 @@ var OpenStackListInstance = (function (JSTACK) {
                         JSTACK.Keystone.params.access = response.access;
                         JSTACK.Keystone.params.currentstate = 2;
 
+                        
+                        // Stop loading animation
+                        stopLoadingAnimation();
+                        
+                        // Create table
                         createTable();
+
+                        // Get instance list and draw it in the table
                         getInstanceList();
                     },
                     onFailure: function (response) {
@@ -249,6 +259,26 @@ var OpenStackListInstance = (function (JSTACK) {
             });
         }
 
+    }
+
+    function startLoadingAnimation () {
+
+        // Reference size is the smaller between height and width
+        var referenceSize = (window.innerWidth < window.innerHeight) ? window.innerWidth : window.innerHeight;
+        var font_size = referenceSize / 4;
+
+        // Calculate loading icon size
+        $('.loading i').css('font-size', font_size);
+
+        // Show
+        $('.loading').removeClass('hide');
+
+    }
+
+    function stopLoadingAnimation () {
+
+        // Hide
+        $('.loading').addClass('hide');
     }
 
     function getDisplayableAddresses(addresses) {
